e7d6d9a2eaa436c45c627b86f6d67945f7fd5801
leon3108 <maxime.noel@epitech.eu>
2024-08-09 17:50:48 +0200

    feat: create python interpretor if not created

--- vscode/src/slither.ts
+++ vscode/src/slither.ts
@@ -1,15 +1,30 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
 import * as path from 'path';
 import { TextDecoder } from 'util';
-import { ExtensionContext, Uri, workspace } from 'vscode';
+import { ExtensionContext, Uri, window, workspace } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
 export async function createSlitherClient(context: ExtensionContext): Promise<LanguageClient> {
+  const venvPath = context.asAbsolutePath(path.join('..', 'osmium-vyper-env'));
+  const pythonPath = path.join(venvPath, 'bin', 'python');
+
+  if (!fs.existsSync(pythonPath)) {
+    window.showErrorMessage(
+      `L'interpréteur Python n'a pas été trouvé à l'emplacement : ${pythonPath}. Installation en cours ...`,
+    );
+    try {
+      execSync(`python3 -m venv ${venvPath}`);
+    } catch (error: any) {
+      window.showErrorMessage(`Erreur lors de la création de l'environnement virtuel : ${error.message}`);
+      throw new Error(`Erreur lors de la création de l'environnement virtuel : ${error.message}`);
+    }
+  } else {
+    window.showInformationMessage("L'interpréteur Python trouvé");
+  }
+
   // The server is implemented in node
   const serverScript = context.asAbsolutePath(path.join('dist', 'slither-server', 'server.py'));
-  const pythonPath = context.asAbsolutePath(path.join('..', 'osmium-vyper-env', 'bin', 'python')); // Chemin vers l'interpréteur Python de l'environnement virtuel
-
-  console.log('serverScript', serverScript);
-  console.log('pythonPath', pythonPath);
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
